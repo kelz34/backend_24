3ebb53c2361709411271a13ff75b12f048533863
Kelvin <kelzlaur34@gmail.com>
2023-11-23 12:20:07 -0500

    moved project into dir

--- blog/blogproject/settings.py
+++ blog/blogproject/settings.py
@@ -122,9 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = 'static/'
-
-STATIC_ROOT = 'hw24_blog_app_api' / 'staticfiles'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
